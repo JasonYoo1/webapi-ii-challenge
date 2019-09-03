da32a97c2ec0c2c27d4622d643f483ac62327697
JasonYoo1 <jyoojs@gmail.com>
2019-09-03 15:14:13 -0700

    all posts req success

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const server = express();
 
 server.use(express.json());
 
-// Server test on the root url without /api extension.
+
 server.get('/', (req, res) => {
   res.status(200).json({message: 'it works?'})
-})
+});
+
+//all posts
+server.get('/api/posts', (req, res) => {
+  db.find() 
+    .then(result => res.status(201).json(result))
+    .catch(error => res.status(500).json({ error: "The posts information could not be retrieved." }))
+});
+
 
 module.exports = server
